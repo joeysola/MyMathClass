eca3066f85cdf493dbc6bf04b59c816c6b008f08
joeysola <joeysolak@gmail.com>
2016-11-23 19:48:28 -0500

    Update 1
    Create MyMathClass

---
+++ MyMathClass/src/mymathclass/MyMathClass.java
@@ -0,0 +1,22 @@
+/*
+ * Name: Joey Solak
+ * Date:
+ * Class: CIT-130
+ * Purpose Of The File:
+ */
+package mymathclass;
+
+/**
+ *
+ * @author Joey Solak
+ */
+public class MyMathClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
